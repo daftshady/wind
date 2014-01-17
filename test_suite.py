98f306791bd748c701e323ae58ffeac11df9ccfe
daftshady <daftonshady@gmail.com>
2014-01-18 02:31:51 +0900

    added flexible deque test cases

--- test_suite.py
+++ test_suite.py
@@ -1,7 +1,53 @@
 #!/usr/bin/python
 
-"""
-Tests for wind
-"""
+"""Tests for wind"""
+
+import unittest
+from wind.web.datastructures import FlexibleDeque
 
 
+class StreamTestCase(unittest.TestCase):
+    """Tests for modules in web.stream"""
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_socket_read_bytes(self):
+        pass
+
+
+class DatastructuresTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+    
+    def test_flexible_deque_gather(self):
+        # Gather left
+        q = FlexibleDeque(['y', '-', 'combinator', '...'])
+
+        q.gather(12)
+        assert q == FlexibleDeque(['y-combinator', '...'])
+
+        q.gather(13)
+        assert q == FlexibleDeque(['y-combinator.', '..'])
+        
+        q.gather(30)
+        assert q == FlexibleDeque(['y-combinator...'])
+
+        # Gather right
+        q = FlexibleDeque(['Park', ' ', 'il', 'su', '...'])
+
+        q.gather(9, left=False)
+        assert q == FlexibleDeque(['Par', 'k ilsu...'])
+        
+        q.gather(30, left=False)
+        assert q == FlexibleDeque(['Park ilsu...'])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
