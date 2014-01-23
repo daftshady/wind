91b1899051c3ae9e941fffcb6491af24705a0231
daftshady <daftonshady@gmail.com>
2014-01-23 21:59:34 +0900

    making tcpserver workable

--- wind/socketserver.py
+++ wind/socketserver.py
@@ -1,4 +1,3 @@
-# -*- coding:utf-8 -*-
 """
 
     wind.socketserver
@@ -11,8 +10,9 @@
 
 import socket
 import errno
-from wind.exceptions import SocketException
-from wind.looper import PollLooper
+from wind.looper import Looper
+from wind.poll import PollEvents
+from wind.exceptions import SocketError, ServerError
 
 
 # Defines frequently used collection of socket.error
@@ -27,18 +27,37 @@ class BaseServer(object):
 
 
 class TCPServer(BaseServer):
-    """Non-blocking, single-threaded TCP Server"""
+    """Non-blocking, single-threaded TCP Server implementation.
+
+    Methods for the caller:
+
+    - __init__(looper=None)
+    - bind(address, port)
+    - listen(address, port)
+    - attach_sockets(sockets=[])
+    - run_simple(address, port)  
+    
+    Methdos should be overrided
+
+    - _connection_handler(conn, address)
+    
+
+    """
     # It will consume kernel resource
     backlog_size = 128
 
     def __init__(self, looper=None):
         """Initialize tcp server.
 
-        :param looper: io looper for simply running this server
+        :param looper: `Looper` for simply running this server
         """
-        self.looper = looper or PollLooper.instance()
-        self.socket = None
+        self.looper = looper or Looper.instance()
+        self._sockets = []
     
+    def bind(self, address, port):
+        """Binds socket on specified address, port"""
+        self._sockets.append(self._bind_socket(address, port))
+
     def _bind_socket(
         self, address, port, family=socket.AF_INET, 
         socket_type=socket.SOCK_STREAM):
@@ -48,14 +67,12 @@ class TCPServer(BaseServer):
         Sockets should be bound all ip address if `address` is 
         a hostname.
         """
-        if self.socket is not None:
+        if self._sockets:
             raise SocketException('Socket is already bound')
         
         try:
             socket_ = socket.socket(family, socket_type)
         except socket.error as e:
-            if e.args[0] == errno.EAFNOSUPPORT:
-                continue
             raise SocketException('Socket creation failed')
         
         socket_.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -63,20 +80,57 @@ class TCPServer(BaseServer):
         socket_.bind((address, port))
         socket_.listen(self.backlog_size)
         return socket_
-    
-    def bind(self, address, port):
-        """Binds socket"""
-        self.socket = self._bind_socket(address, port)
-    
+   
     def listen(self, address, port):
-        """Binds socket and actually run looper"""
+        """Binds socket and actually attach this server on looper"""
         self.bind(address, port)
+        self._bind_to_looper()
 
-    def run_simple(self, address, port, single_handler):
+    def run_simple(self, address, port):
         """Simply run tcp server with single-process"""
-        pass
+        self.listen(address, port)
+        self.looper.run()
+
+    def attach_sockets(self, sockets=[]):
+        """Attach extra sockets to tcp server instance"""
+        self._bind_to_looper(sockets=sockets)
+   
+    def _bind_to_looper(self, sockets=[]):
+        """Bind sockets to looper.
+        This must be called before actual running of server
+        because initialized sockets must be bound to looper
+        
+        @param sockets: Extra sockets to be bound on this server.
+        """
+        self._sockets.extend(sockets)
+        for socket_ in self._sockets:
+            self._attach_accept_handler(socket_, self._connection_handler)
+                
+    def _attach_accept_handler(self, socket_, callback):
+        """Attach `_event_handler` to socket"""
+        def _accept_handler(self, fd, event_mask):
+            """Handle socket accept and execute callback"""
+            while True:
+                try:
+                    conn, address = socket_.accept()
+                except socket.error as e:
+                    if e.args[0] in EWOULDBLOCK:
+                        return
+                    raise
+            
+                try:
+                    callback(conn, address)
+                except TypeError:
+                    raise ServerError('Accept handler callback is not vaild form')
+        self.looper.attach_handler(
+            socket_.fileno(), PollEvents.READ, self._connection_handler)
+    
+    def _connection_handler(self, conn, address):
+        raise NotImplementedError
 
 
 class UDPServer(BaseServer):
     # TODO : Planned to be implemented later.
     pass
+
+
