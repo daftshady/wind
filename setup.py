ffe9242f63b134a988a66eb11c1c5f37712a7f1d
daftshady <daftonshady@gmail.com>
2014-03-19 00:10:10 +0900

    added simple setup script

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+packages = [
+    'wind',
+    'wind.web'
+    ]
+
+requires = []
+
+setup(
+    name='wind',
+    version='0.1',
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    author='daftshady',
+    author_email='daftonshady@gmail.com',
+    license=open('LICENSE').read(),
+    description='Web framework based on async networking server',
+    install_requires=requires
+)
+
