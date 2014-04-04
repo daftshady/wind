e12201a428b7885c7f80cc4fd18ffc2e1508a7fa
daftshady <daftonshady@gmail.com>
2014-04-04 14:55:19 +0900

    docs for HelloResource

--- wind/web/app.py
+++ wind/web/app.py
@@ -26,23 +26,30 @@ class WindApp(object):
     """Wind web application
     We expect that our app usage code will be like this, and it works now.
     This usage interface is made for the purpose of testing `performance`.
-    Therefore, it may be fully revised.
+    It may be revised later.
 
     `hello wind!` Example::
 
-        from wind.web.app import WindApp, path
         from wind.web.httpserver import HTTPServer
+        from wind.web.app import WindApp, path, Resource
 
         def hello_wind(request):
             return 'hello wind!'
 
+        class HelloResource(Resource):
+            def handle_get(self):
+                self.write('hello wind!')
+                self.finish()
+
         app = WindApp([
-                path(hello_wind, route='/', methods=['get'])
+                path(hello_wind, route='/', methods=['get']),
+                path(HelloResource, route='/resource', methods=['get'])
                 ])
         server = HTTPServer(app=app)
         server.run_simple('127.0.0.1', 7000)
 
     """
+
     def __init__(self, urls):
         self._dispatcher = PathDispatcher(urls)
 
